d73c649d4ed73d69e647986ccce59372b5fe2257
Edvaldo Silva <edvaldodeveloper@gmail.com>
2020-07-26 22:33:02 -0300

    Sistema de templates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def hello_blog(request):
+    lista = [
+        'Django', 'Python', 'Git',
+        'Html', 'Banco de dados', 'Linux'
+    ]
+    list_posts = Post.objects.all()
+
+    data = {
+        'name': 'Curso de Django 3',
+        'lista_tecnologias': lista,
+        'posts': list_posts}
+
+    return render(request, 'index.html', data)
